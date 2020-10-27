fc0daec036c6c49a286513c0a53eca4e8bf7d62b
dhakalsamundra <seapush02@gmail.com>
2020-10-27 21:36:13 +0200

    add regsiter page

---
+++ client/src/pages/Register.js
@@ -0,0 +1,102 @@
+import React, { useState } from 'react'
+import { Row, Col, Form, Button } from 'react-bootstrap'
+import { gql, useMutation } from '@apollo/client';
+
+const REGISTER_USER = gql`
+  mutation register(
+      $username: String!
+      $email: String!
+      $password: String!
+      $confirmPassword: String! 
+      ) {
+    register(
+        username: $username email: $email password: $password confirmPassword: $confirmPassword
+    ) {
+      username email createdAt
+    }
+  }
+`;
+
+export default function Register() {
+  const [variables, setVariables] = useState({
+    email: '',
+    username: '',
+    password: '',
+    confirmPassword: '',
+  })
+  const [errors, setErrors] = useState({})
+
+  const [createUser, { loading }] = useMutation(REGISTER_USER, {
+      update(_, result){
+          console.log(result)
+      },
+      onError(error){
+        //   console.log(error.graphQLErrors[0].extensions.errors)
+          setErrors(error.graphQLErrors[0].extensions.errors)
+      }
+  })
+
+  const submitRegisterForm = (e) => {
+    e.preventDefault()
+
+    createUser({ variables })
+  }
+
+  return (
+    <Row className="bg-white py-5 justify-content-center">
+      <Col sm={8} md={6} lg={4}>
+        <h1 className="text-center">Register</h1>
+        <Form onSubmit={submitRegisterForm}>
+          <Form.Group>
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              value={variables.email}
+              onChange={(e) =>
+                setVariables({ ...variables, email: e.target.value })
+              }
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              type="text"
+              value={variables.username}
+              onChange={(e) =>
+                setVariables({ ...variables, username: e.target.value })
+              }
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              value={variables.password}
+              onChange={(e) =>
+                setVariables({ ...variables, password: e.target.value })
+              }
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Confirm password</Form.Label>
+            <Form.Control
+              type="password"
+              value={variables.confirmPassword}
+              onChange={(e) =>
+                setVariables({
+                  ...variables,
+                  confirmPassword: e.target.value,
+                })
+              }
+            />
+          </Form.Group>
+          <div className="text-center">
+            <Button variant="success" type="submit">
+              Register
+            </Button>
+          </div>
+        </Form>
+      </Col>
+    </Row>
+  )
+}
