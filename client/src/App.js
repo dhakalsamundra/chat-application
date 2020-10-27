5081978ce4866831f5016581154e10993f40b490
dhakalsamundra <seapush02@gmail.com>
2020-10-27 22:49:52 +0200

    add route

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,26 @@
+import React from 'react'
 import { Container } from "react-bootstrap";
-
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ApolloProvider from "./ApolloProvider";
 
 import "./App.scss";
 import Register from "./pages/Register";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
 
 function App() {
   return (
     <ApolloProvider>
-      <Container>
-        <Register />
-      </Container>
+      <BrowserRouter>
+        <Container className="pt-5">
+          <Switch>
+            <Route path="/register" component={Register} />
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </Container>
+      </BrowserRouter>
     </ApolloProvider>
   );
 }
