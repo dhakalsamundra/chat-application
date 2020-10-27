90b02641583bbce33a548514f8becc4d2ea2ab42
dhakalsamundra <seapush02@gmail.com>
2020-10-27 12:56:00 +0200

    add schema

--- server.js
+++ server.js
@@ -10,10 +10,11 @@ const resolvers = require('./graphql/resolvers')
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: (ctx) => ctx,
 });
 
 server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
+  console.log(`Server ready at ${url}`);
 
   sequelize.authenticate()
   .then(()=> console.log('Database connected..'))
