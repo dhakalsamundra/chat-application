6e401e68e45b09cbd5a6178c893b45930f0575ba
dhakalsamundra <seapush02@gmail.com>
2020-10-27 12:56:51 +0200

    add login query and register mutation

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -4,8 +4,20 @@ module.exports =  gql`
   type User {
     username: String!
     email: String!
+    token: String
+    createdAt: String!
   }
   type Query {
     getUsers: [User]!
+    login(username:String!, password: String!): User!
+  }
+  type Mutation{
+    register(
+      username: String! 
+      email: String! 
+      password: String! 
+      confirmPassword: String!
+      ): User!
   }
 `;
+
