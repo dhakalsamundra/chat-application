98e4f44eecb4ecdf61a40b0a7a57d41a0f6efa9f
dhakalsamundra <seapush02@gmail.com>
2020-10-28 12:12:48 +0200

    use dispatch to set the token while login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,6 +3,7 @@ import { Row, Col, Form, Button } from 'react-bootstrap'
 import { gql, useLazyQuery } from '@apollo/client'
 import { Link } from 'react-router-dom'
 
+import {  useAuthDispatch } from '../context/auth'
 const LOGIN_USER = gql`
   query login($username: String!, $password: String!) {
     login(username: $username, password: $password) {
@@ -20,13 +21,14 @@ export default function Login(props) {
     password: '',
   })
   const [errors, setErrors] = useState({})
+  const dispatch = useAuthDispatch()
 
   /*lazyQuery works same as mutation but it use as a query. 
     returns a function tuple that can call whenever one is ready to execute the query*/
   const [loginUser, { loading }] = useLazyQuery(LOGIN_USER, {
     onError: (err) => setErrors(err.graphQLErrors[0].extensions.errors),
     onCompleted(data) {
-      localStorage.setItem('token', data.login.token)
+      dispatch({ type: 'LOGIN', payload: data.login})
       props.history.push('/')
     },
   })
