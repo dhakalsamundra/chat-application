ad5cc80b9ec633bae0c74a4e1e07eb976dbdf801
dhakalsamundra <seapush02@gmail.com>
2020-10-28 10:34:51 +0200

    add type, query and mutation for message

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -7,9 +7,18 @@ module.exports =  gql`
     token: String
     createdAt: String!
   }
+  type Message {
+    uuid: String!
+    content: String!
+    from: String!
+    to: String!
+    createdAt: String!
+
+  }
   type Query {
     getUsers: [User]!
     login(username:String!, password: String!): User!
+    getMessages(from:String!): [Message]!
   }
   type Mutation{
     register(
@@ -18,6 +27,7 @@ module.exports =  gql`
       password: String! 
       confirmPassword: String!
       ): User!
+    sendMessage(to:String! content:String!): Message!
   }
 `;
 
