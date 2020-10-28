21427f0d0a139c90dce25ef46d58248b872bb0bf
dhakalsamundra <seapush02@gmail.com>
2020-10-28 12:15:14 +0200

    wrap with private route component

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
 import React from 'react'
 import { Container } from "react-bootstrap";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Switch } from "react-router-dom";
 import ApolloProvider from "./ApolloProvider";
 
 import "./App.scss";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import { AuthProvider } from './context/auth'
+import PrivateRoute from './util/PrivateRoute'
 
 
 function App() {
   return (
     <ApolloProvider>
+      <AuthProvider>
       <BrowserRouter>
         <Container className="pt-5">
           <Switch>
-            <Route path="/register" component={Register} />
-            <Route exact path="/" component={Home} />
-            <Route path="/login" component={Login} />
+            <PrivateRoute path="/register" component={Register} guest />
+            <PrivateRoute exact path="/" component={Home} authenticated />
+            <PrivateRoute path="/login" component={Login} guest />
           </Switch>
         </Container>
       </BrowserRouter>
+      </AuthProvider>
     </ApolloProvider>
   );
 }
