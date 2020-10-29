0626d02728de72e3a6a6c1576178b0a2b88609ad
dhakalsamundra <seapush02@gmail.com>
2020-10-29 09:40:31 +0200

    import user from context and implement

--- client/src/pages/home/Messages.js
+++ client/src/pages/home/Messages.js
@@ -2,6 +2,9 @@ import React, { useEffect } from 'react'
 import { gql, useLazyQuery } from '@apollo/client'
 import { Col } from 'react-bootstrap'
 
+import { useMessageDispatch, useMessageState } from "../../context/message";
+import EachMessage from './EachMessage'
+
 
 const GET_MESSAGES = gql`
 query getMessages($from: String!){
@@ -10,28 +13,52 @@ query getMessages($from: String!){
 }
 }`
 
-export default function Messages({ selectedUser }) {
+export default function Messages() {
+  const { users } = useMessageState()
+  const dispatch = useMessageDispatch()
+
+  const selectedUser = users?.find((u) => u.selected === true)
+  const messages = selectedUser?.messages
 
   const [
     getMessages,{
-     loading, data: messagesData },] = useLazyQuery(GET_MESSAGES)
+     loading: messagesLoading, data: messagesData },] = useLazyQuery(GET_MESSAGES)
 
   useEffect(() => {
-    if (selectedUser) {
-      getMessages({ variables: { from: selectedUser } })
+    if (selectedUser && !selectedUser.messages) {
+      getMessages({ variables: { from: selectedUser.username } })
     }
     // eslint-disable-next-line
   }, [selectedUser])
 
+  useEffect(() => {
+    if (messagesData) {
+      dispatch({
+        type: 'SET_USER_MESSAGES',
+        payload: {
+          username: selectedUser.username,
+          messages: messagesData.getMessages,
+        },
+      })
+    }
+    // eslint-disable-next-line
+  }, [messagesData])
+
+  let selectedChatMarkup
+  if (!messages && !messagesLoading) {
+    selectedChatMarkup = <p>Select a friend</p>
+  } else if (messagesLoading) {
+    selectedChatMarkup = <p>Loading..</p>
+  } else if (messages.length > 0) {
+    selectedChatMarkup = messages.map((msg) => (
+      <EachMessage key={msg.uuid} message={msg} />
+    ))
+  } else if (messages.length === 0) {
+    selectedChatMarkup = <p>You are now connected! send your first message!</p>
+  }
   return (
     <Col xs={8}>
-          {messagesData && messagesData.getMessages.length > 0 ? (
-            messagesData.getMessages.map((msg) => (
-            <p key={msg.uuid}>{msg.content}</p>
-            ))
-          ) : (
-            <p>Messages</p>
-          )}
+         {selectedChatMarkup}
         </Col>
   )
 }
