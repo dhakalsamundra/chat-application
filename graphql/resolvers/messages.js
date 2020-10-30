e2887e7712af97af0561d385314e5e3c66ef4a2d
dhakalsamundra <seapush02@gmail.com>
2020-10-30 14:43:03 +0200

    add reaction function

--- graphql/resolvers/messages.js
+++ graphql/resolvers/messages.js
@@ -1,61 +1,152 @@
-const { UserInputError, AuthenticationError } = require('apollo-server') 
+const {
+  UserInputError,
+  AuthenticationError,
+  ForbiddenError,
+  withFilter,
+} = require('apollo-server')
 const { Op } = require('sequelize')
-const { User, Message } = require('../../models')
 
-module.exports ={
-    Query: {
-        getMessages: async (parent, { from }, { user }) => {
-            try {
-                if(!user) throw new AuthenticationError('Unauthenticated')
-                const restUser = await User.findOne({
-                    where: { username: from}
-                })
-                if(!restUser) throw new UserInputError('User not found')
+const { Message, User, Reaction } = require('../../models')
 
-                const userNames = [user.username, restUser.username]
-                // finding all message between sender and receiver
-                const messages = await Message.findAll({
-                    where: {
-                        from: { [Op.in]: userNames },
-                        to: { [Op.in]: userNames },
-                    },
-                    order: [[ 'createdAt', 'DESC' ]]
-                })
-                return messages
+module.exports = {
+  Query: {
+    getMessages: async (parent, { from }, { user }) => {
+      try {
+        if (!user) throw new AuthenticationError('Unauthenticated')
 
-            } catch (error) {
-                console.log(error)
-                throw error
-            }
-        }
+        const otherUser = await User.findOne({
+          where: { username: from },
+        })
+        if (!otherUser) throw new UserInputError('User not found')
+
+        const usernames = [user.username, otherUser.username]
+
+        const messages = await Message.findAll({
+          where: {
+            from: { [Op.in]: usernames },
+            to: { [Op.in]: usernames },
+          },
+          order: [['createdAt', 'DESC']],
+        })
+
+        return messages
+      } catch (err) {
+        console.log(err)
+        throw err
+      }
     },
-    Mutation: {
-      sendMessage: async (parent, { to, content }, { user }) => {
-        try {
-          if(!user) throw new AuthenticationError('Unauthenticated')
+  },
+  Mutation: {
+    sendMessage: async (parent, { to, content }, { user, pubsub }) => {
+      try {
+        if (!user) throw new AuthenticationError('Unauthenticated')
 
-          const recipient = await User.findOne({
-            where: { username: to}})
-          if(!recipient){
-            throw new UserInputError('User not found')
-          } else if (recipient.username === user.username) {
-            throw new UserInputError('You cant message yourself')
-          }
-          if(content.trim() === ''){
-            throw new UserInputError('Message is empty')
-          }
-          //Sending mesage
-          const message = await Message.create({
-            from: user.username,
-            to,
+        const recipient = await User.findOne({ where: { username: to } })
+
+        if (!recipient) {
+          throw new UserInputError('User not found')
+        } else if (recipient.username === user.username) {
+          throw new UserInputError('You can not message yourself')
+        }
+
+        if (content.trim() === '') {
+          throw new UserInputError('Message is empty')
+        }
+
+        const message = await Message.create({
+          from: user.username,
+          to,
+          content,
+        })
+
+        pubsub.publish('NEW_MESSAGE', { newMessage: message })
+
+        return message
+      } catch (err) {
+        console.log(err)
+        throw err
+      }
+    },
+    reactToMessage: async(_, { uuid, content}, { user, pubsub}) => {
+      const reactions = ['❤️', '😆', '😯', '😢', '😡', '👍', '👎', ' 🤷‍♀️', ' 🤷‍♂️']
+
+      try {
+        // validate reaction content
+        if(!reactions.includes(content)){
+          throw new UserInputError('Invalid reaction')
+        }
+        //Get user
+        const username = user ? user.username : ''
+        user = await User.findOne({ where: { username }})
+        if(!user) throw new AuthenticationError('UnAunthenticated')
+        //Get message
+        const message = await Message.findOne({ where: { uuid}})
+        if(!message) throw new UserInputError('message not found')
+        if(message.from !== user.username && message.to !== user.username){
+          throw new ForbiddenError('UnAuthorized')
+        }
+
+        let reaction = await Reaction.findOne({ where: { messageId: message.id, userId: user.id}})
+
+        if(reaction){
+          //if exists, update it
+          reaction.content = content
+          await reaction.save()
+        } else {
+          //Reaction not found, so create it
+          reaction = await Reaction.create({
+            messageId: message.id,
+            userId: user.id,
             content,
           })
-
-          return message
-        } catch (error) {
-          console.log(error)
-          throw error
         }
+        pubsub.publish('NEW_REACTION', {
+          newReaction: reaction
+        })
+        return reaction
+      } catch (error) {
+        throw error
       }
     }
-  };
+  },
+  Subscription: {
+    newMessage: {
+      subscribe: withFilter(
+        (_, __, { pubsub, user }) => {
+          if (!user) throw new AuthenticationError('Unauthenticated')
+          return pubsub.asyncIterator('NEW_MESSAGE')
+        },
+        ({ newMessage }, _, { user }) => {
+          if (
+            newMessage.from === user.username ||
+            newMessage.to === user.username
+          ) {
+            return true
+          }
+
+          return false
+        }
+      ),
+    },
+
+    newReaction: {
+      subscribe: withFilter(
+        (_, __, { pubsub, user }) => {
+          if (!user) throw new AuthenticationError('Unauthenticated')
+          return pubsub.asyncIterator('NEW_REACTION')
+        },
+        async({ newReaction }, _, { user }) => {
+          const message = await newReaction.getMessage()
+          if (
+            message.from === user.username ||
+            message.to === user.username
+          ) {
+            return true
+          }
+
+          return false
+        }
+      ),
+    },
+  },
+}
