e537b61b372462a58914c1c1a897887ac2271f31
dhakalsamundra <seapush02@gmail.com>
2020-10-30 12:34:44 +0200

    typoerror solve

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -1,26 +1,31 @@
 import React from 'react'
-import { ApolloClient, InMemoryCache , split, ApolloProvider as Provider, createHttpLink} from '@apollo/client';
-
-import { setContext } from '@apollo/client/link/context';
-import { WebSocketLink } from '@apollo/client/link/ws';
-import { getMainDefinition } from '@apollo/client/utilities';
-
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider as Provider,
+  createHttpLink,
+  split,
+} from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+import { WebSocketLink } from '@apollo/client/link/ws'
+import { getMainDefinition } from '@apollo/client/utilities'
 
 let httpLink = createHttpLink({
   uri: 'http://localhost:4000',
-});
+})
 
 const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('token');
+  // // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token')
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    }
+      authorization: token ? `Bearer ${token}` : '',
+    },
   }
-});
+})
+
 httpLink = authLink.concat(httpLink)
 
 const wsLink = new WebSocketLink({
@@ -28,28 +33,28 @@ const wsLink = new WebSocketLink({
   options: {
     reconnect: true,
     connectionParams: {
-      authorization: `Bearer ${localStorage.getItem('token')}`,
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
     },
-  }
-});
+  },
+})
 
 const splitLink = split(
   ({ query }) => {
-    const definition = getMainDefinition(query);
+    const definition = getMainDefinition(query)
     return (
       definition.kind === 'OperationDefinition' &&
       definition.operation === 'subscription'
-    );
+    )
   },
   wsLink,
-  httpLink,
-);
+  httpLink
+)
 
 const client = new ApolloClient({
- link: splitLink,
-  cache: new InMemoryCache()
-});
+  link: splitLink,
+  cache: new InMemoryCache(),
+})
 
-export default function ApolloProvider(props){
-    return <Provider client={client}{...props} />
+export default function ApolloProvider(props) {
+  return <Provider client={client} {...props} />
 }
