6e3a1d8da1a93595f95f82b129200bb04f35e37a
dhakalsamundra <seapush02@gmail.com>
2020-10-30 12:45:07 +0200

    modified

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,37 @@
 import React from 'react'
-import { Container } from "react-bootstrap";
-import { BrowserRouter, Switch } from "react-router-dom";
-import ApolloProvider from "./ApolloProvider";
+import { Container } from 'react-bootstrap'
+import { BrowserRouter, Switch } from 'react-router-dom'
 
-import "./App.scss";
-import Register from "./pages/Register";
-import Home from "./pages/home/Home";
-import Login from "./pages/Login";
+import ApolloProvider from './ApolloProvider'
+
+import './App.scss'
+
+import Home from './pages/home/Home'
+import Register from './pages/Register'
+import Login from './pages/Login'
+
 import { AuthProvider } from './context/auth'
 import { MessageProvider } from './context/message'
 import PrivateRoute from './util/PrivateRoute'
-
 
 function App() {
   return (
     <ApolloProvider>
       <AuthProvider>
         <MessageProvider>
-      <BrowserRouter>
-        <Container className="pt-5">
-          <Switch>
-            <PrivateRoute path="/register" component={Register} guest />
-            <PrivateRoute exact path="/" component={Home} authenticated />
-            <PrivateRoute path="/login" component={Login} guest />
-          </Switch>
-        </Container>
-      </BrowserRouter>
-      </MessageProvider>
+          <BrowserRouter>
+            <Container className="pt-5">
+              <Switch>
+                <PrivateRoute exact path="/" component={Home} authenticated />
+                <PrivateRoute path="/register" component={Register} guest />
+                <PrivateRoute path="/login" component={Login} guest />
+              </Switch>
+            </Container>
+          </BrowserRouter>
+        </MessageProvider>
       </AuthProvider>
     </ApolloProvider>
-  );
+  )
 }
 
-export default App;
+export default App
